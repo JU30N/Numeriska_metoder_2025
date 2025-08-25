03102c8425fb925433b679225e536140ba7894db
JU30N <joongwon.hong@gmail.com>
2025-08-25 19:01:03 +0200

    s

--- Forelasningar/F1.py
+++ Forelasningar/F1.py
@@ -5,17 +5,17 @@ import os
 def clear_console():
     os.system('clear')
 
-f = lambda x: 2**x**3 + 1
+f = lambda x: 2*x**3 + 1
 
 def plotta(f, a, b):
     fig, ax = plt.subplots()
     x_vec = np.linspace(a, b, 200)
     y_vac = f(x_vec)
-    plt.plot(x_vec, y_vac, color='red', lable='')
-    ax.set_xlablel('x', fontsize=14)
-    ax.set_ylablel('y', fontsize=14)
+    plt.plot(x_vec, y_vac, color='red', label='')
+    ax.set_xlabel('x', fontsize=14)
+    ax.set_ylabel('y', fontsize=14)
     ax.tick_params (labelsize=14)
-    plt.grid(True)
+    plt.show()
 
 def main():
     clear_console()
