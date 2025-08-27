51a319708877808166bafd0f816a66387cba07bd
JU30N <joongwon.hong@gmail.com>
2025-08-27 19:08:58 +0200

    först uppgiften gjord

---
+++ Laboration_1/U1.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+#a) svar 2st
+# def clear_console():
+#     os.system('clear')
+    
+# f = lambda x, L:(8/3)*(x/L) - 3*(x/L)**2 + (1/3)*(x/L)**3 - (2/3)*np.sin(np.pi * x / L)
+
+# def plotta(f, a, b):
+#     fig, ax = plt.subplots()
+#     x = np.linspace(a, b, 100)#mellan a och b med 100 punkter
+#     y = f(x, L=1)
+#     ax.plot(x, y)#plotta
+#     ax.set_xlabel('x')#sätta som x
+#     ax.set_ylabel('f(x)')#sätta som y
+#     ax.set_title('Plot of the function f(x)')
+#     plt.grid()#visa grid
+#     plt.show()
+    
+# def main():
+#     clear_console()
+#     plotta(f, 0, 1)#mellan 0 och 1
+    
+
+# if __name__ == '__main__':
+#     main()
+
+#b)
