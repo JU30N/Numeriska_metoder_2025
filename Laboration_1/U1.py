f32164ec74b034730ed73fd50ba0fccbbff2cce5
JU30N <joongwon.hong@gmail.com>
2025-08-29 13:02:27 +0200

    done with all and changed name to U1...

--- Laboration_1/U1.py
+++ Laboration_1/U1.py
@@ -3,7 +3,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 #a)
-def Ua():
+def U1a():
     def f(x, L):
         return (8/3) * (x / L) - 3 * (x / L)**2 + (1/3) * (x / L)**3 - (2/3) * np.sin(np.pi * x / L)
 
@@ -21,7 +21,7 @@ def Ua():
     plt.show()
 
 #b)svar: man kan hitta en. 
-def Ub():
+def U1b():
 
     def fipforloop(L, x0, tol, max_iter):
     # Initiera loopen
@@ -47,13 +47,13 @@ def Ub():
     #print(g_prim(0.8,1))
     #print(g_prim(0.3,1))
 
-
-def Uc():
+def U1c():
     #samma som Ub()
     return 
 
-def Ud():#svar den hittar båda då x0 är tillräckligt nära 
-    x0 = 0.3
+#c)svar: hittar vid 0.3
+def U1d():
+    x0 = 0.2
     tol = 1E-10
     max_iter = 1000
 
@@ -78,12 +78,64 @@ def Ud():#svar den hittar båda då x0 är tillräckligt nära
     root, iterations = newton_method(x0,tol,max_iter)
     print('\n')
     print(f"Fixed point: {root:.8f}, after {iterations} iterations")
+    print(root)
+    print(iterations)
+
+#e)svar: båda konvergerar vid 0.8
+def U1e():
+
+    def newton_method(x0, tol, max_iter):
+        f = lambda x:(8/3)*(x) - 3*(x)**2 + (1/3)*(x)**3 - (2/3)*np.sin(np.pi * x)
+        f_prim = lambda x: (8/3) - 6*x + x**2 - (2*np.pi/3) * np.cos(np.pi*x)
+        x = x0
+        n_iter = 0
+        Delta_x = tol + 1  
+        Delta_x_n_list = []
+        while Delta_x > tol:
+            n_iter += 1
+            xnew = x - (f(x)/(f_prim(x)))
+            Delta_x = np.abs(xnew - x)
+            #print(Delta_x)
+            x = xnew
+            #print(f"Iteration {n_iter}: x = {x}, x_new = {Delta_x}")
+            Delta_x_n_list.append(Delta_x)
+            if n_iter > max_iter:
+                raise RuntimeError
+        return Delta_x_n_list, n_iter
+    
+    def FPI_method(x0, tol, maxiter):
+        g = lambda x: ((3/8)) * (3*(x)**2 - (1/3)*(x)**3 + (2/3)*np.sin(np.pi*x))
+        Delta_x = tol + 1
+        x = x0
+        n_inter = 0
+        Delta_x_FPI_list = []
+        while Delta_x > tol:
+            n_inter += 1
+            xnew = g(x)
+            Delta_x = np.abs(xnew - x)
+            Delta_x_FPI_list.append(Delta_x)
+            if n_inter > maxiter:
+                raise RuntimeError
+            else:
+                x = xnew
+        return Delta_x_FPI_list, n_inter
+
+    x0 = 0.8
+    tol = 1e-10
+
+    Delta_x_newton,n_iter_n = newton_method(x0, tol, 1000)
+    #print(Delta_x_newton)
+    Delta_x_FPI, n_iter_FPI= FPI_method(x0, tol, 1000)
+
+
+    plt.semilogy(range(1, n_iter_n+1), Delta_x_newton,color = 'r')
+    plt.semilogy(range(1, n_iter_FPI+1),Delta_x_FPI,  color = 'b')
+    plt.grid(True)
+    plt.show()
 
 
 
-
-
-
-#Ua()
-#Ub()
-Ud()
+#U1a()
+#U1b()
+#U1d()
+U1e()
