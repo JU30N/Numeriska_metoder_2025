89e0274860645d8b305d7fbe35d1f686ce7ce34f
JU30N <joongwon.hong@gmail.com>
2025-09-24 09:53:56 +0200

    .

--- Övningar/Ö4.py
+++ Övningar/Ö4.py
@@ -1,23 +1,25 @@
 import numpy as np
-import matplotlib.pyplot as plt
-# Givna data
-t = np.array([0,1/4,1/2,3/4])
-y = np.array([1,3,2,1/2])
+x0 = 2
+tol = 1E-10
+max_iter = 3
 
-def minstakvadratmetoden(t, y):
-    n = len(t)
-    y = y.reshape(n,1)
+def newton_method(x0, tol, max_iter):
+    f = lambda x:5*x + x**2 - np.cos(2*x)
+    f_prim = lambda x: 5 + 2*x + 2*np.sin(2*x)
+    x = x0
+    n_iter = 0
+    Delta_x = tol + 1  
+    while Delta_x > tol:
+        n_iter += 1
+        xnew = x - (f(x)/(f_prim(x)))
+        Delta_x = np.abs(xnew - x)
+        print(Delta_x)
+        x = xnew
+        print(f"Iteration {n_iter}: x = {x}, x_new = {Delta_x}")
 
-    # Bygg designmatrisen
-    A = np.column_stack([np.ones_like(t), np.cos(2*np.pi*t), np.sin(2*np.pi*t)])
 
-    # Lös normalekvationerna
-    AT = A.T
-    a = np.linalg.solve(AT @ A, AT @ y)
+        if n_iter > max_iter:
+            raise RuntimeError
+    return x, n_iter
 
-    return a
-
-B = np.matmul(A.transpose(), A)#multiplicera matrisen AT med A 
-b = np.matmul(A.transpose(), y)
-
-C = np.linalg.solve(B, b)
+newton_method(x0, tol, max_iter)
